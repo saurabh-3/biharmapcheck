2ccd4832914584ddda77fd251fb87664117257f1
saurabh kumar <saurabh.kumar.mst16@itbhu.ac.in>
2020-06-01 23:11:35 +0530

    change url

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -13,7 +13,7 @@ function init() {
   var polyURL =
     "https://docs.google.com/spreadsheets/d/17ZKKpTZ5S0LxWKMk5vsl0Hxd7QLqXqCG9v2GiX_XQps/edit?usp=sharing";
   var pointsURL =
-    "https://docs.google.com/spreadsheets/d/1kjJVPF0LyaiaDYF8z_x23UulGciGtBALQ1a1pK0coRM/edit?usp=sharing";
+    "https://docs.google.com/spreadsheets/d/17ZKKpTZ5S0LxWKMk5vsl0Hxd7QLqXqCG9v2GiX_XQps/edit?usp=sharing";
 
   Tabletop.init({ key: polyURL, callback: addPolygons, simpleSheet: true });
   Tabletop.init({ key: pointsURL, callback: addPoints, simpleSheet: true }); // simpleSheet assumes there is only one table and automatically sends its data
